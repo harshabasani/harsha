4bfb403380340af980a8b45c7b5ad3d983c17cb6
harshabasani <harshabasani@users.noreply.github.com>
2016-09-16 17:49:56 +0530

    Add files via upload

---
+++ Survey/src/main/webapp/app.js
@@ -0,0 +1,78 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'template.html',
+		link: function(scope, elem, attrs) {
+			scope.start = function() {
+				scope.id = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+		
+
+			scope.nextQuestion = function() {
+				scope.id++;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question: "Which is your favourite color?",
+			options: ["Red", "Blue", "Green", "Burgundy"],
+			
+		},
+		{
+			question: "What is your favourite cuisine?",
+			options: ["Chinese", "Mughlal", "South Indian", "Italian"],
+			
+		},
+		{
+			question: "What is your preferred Mobile OS?",
+			options: ["Android", "iOS", "Windows", "Firefox OS"],
+			
+		},
+		{
+			question: "Which is the most popular consumer brand in India?",
+			options: ["GSK", "ITC", "Pidlite", "Amul"],
+			
+		}
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
